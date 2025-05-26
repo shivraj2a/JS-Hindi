977796ce3c2e17d37a71605fccdc494981a516c8
shivraj2a <shivrajmohite1903@gmail.com>
2025-05-26 13:50:36 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Shiv"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1 ; false => 0
+// null => 0
+// undefined => NaN
+
+let IsloggedIn = 1
+let boolean = Boolean(IsloggedIn)
+console.log(boolean)
+
+// 1 => true ; 0 => false
+// "" => false
+// "shiv" => true
+
+let Number = 33
+let string = String(Number)
+console.log(typeof string)
+console.log(string)
