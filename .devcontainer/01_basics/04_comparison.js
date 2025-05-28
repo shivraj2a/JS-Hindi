d66417ced2989ce2d11a16f9cf941b143eb6a6fe
shivraj2a <shivrajmohite1903@gmail.com>
2025-05-28 13:08:27 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);         //=>true
+// console.log(2 < 1);         //=>false
+// console.log(2 >= 1);        // => t
+// console.log(2 <= 1);        //=> f
+// console.log(2 == 1);        //=> f
+// console.log(2 != 1);        //=> t
+// console.log(3 != 3);        //=> f
+// console.log(1 == 1);        //=> t
+
+// console.log("2" > 1);          //here the data type is not same
+// console.log("2" >= 1);         //"2" is string & 1 is a number
+// console.log("3" <= 2);  // always compare with same datatype or gets false answers
+
+// console.log(null < 0);          // null is diffrent datatype
+// console.log(null == 0); // here comparison convert null to a number,treating it as a 0
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+
+// use (===) for strict checking
+console.log("2" === "2")
