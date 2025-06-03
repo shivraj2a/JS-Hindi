d5fb31f62ed353a73c7eb430447da7e2df65f3be
shivraj2a <shivrajmohite1903@gmail.com>
2025-06-03 09:18:10 +0000

    strings_me

--- .devcontainer/01_basics/05_strings_me.js
+++ .devcontainer/01_basics/05_strings_me.js
@@ -1,14 +1,39 @@
-const name = "Shiv"
-const num = 50
+// const name = "Shiv123"
+// const num = 50
 // console.log(name + num);
 
 // console.log(`hello my name is ${name} and my favourite num is ${num}, all i need is to learn javascript`);
 
 const newname = new String("Shivraj Mohite")
-console.log(newname);
-console.log(newname.charAt(6));
-console.log(newname.__proto__);
-console.log(newname.length);
-console.log(newname.toUpperCase());
-console.log(newname.toLowerCase());
+// console.log(newname);
+// console.log(newname.charAt(6));
+// console.log(newname.__proto__);
+// console.log(newname.length);
+// console.log(newname.toUpperCase());
+// console.log(newname.toLowerCase());
+
+const Rname = newname.substring(0.5);
+// console.log(Rname);
+
+const Sname = newname.slice(-10, 8);
+// console.log(Sname);
+
+const ASname = "     Shiv Mohite     "
+console.log(ASname.trim());
+
+const SSname ="https://shivraj@20mohite@.com"
+console.log(SSname.replace("@20","%20"))
+console.log(SSname.includes('shivraj'));
+console.log(SSname.split("@"));
+
+
+
+
+
+
+
+
+
+
+
 
