8b50971f3ab1d2a94b952fddf190b919a2713268
shivraj2a <shivrajmohite1903@gmail.com>
2025-06-03 08:39:37 +0000

    strings_me

---
+++ .devcontainer/01_basics/05_strings_me.js
@@ -0,0 +1,14 @@
+const name = "Shiv"
+const num = 50
+// console.log(name + num);
+
+// console.log(`hello my name is ${name} and my favourite num is ${num}, all i need is to learn javascript`);
+
+const newname = new String("Shivraj Mohite")
+console.log(newname);
+console.log(newname.charAt(6));
+console.log(newname.__proto__);
+console.log(newname.length);
+console.log(newname.toUpperCase());
+console.log(newname.toLowerCase());
+
