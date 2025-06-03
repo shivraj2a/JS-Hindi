9dad623e23e9e1a4e9bbab308b7725595cc1d151
shivraj2a <shivrajmohite1903@gmail.com>
2025-06-03 08:39:15 +0000

    stackandheap

---
+++ .devcontainer/01_basics/stackandheap.js
@@ -0,0 +1,19 @@
+let youtubename = "shivmohite"
+let anothername = youtubename
+// console.log(anothername);
+anothername = "vasudange"
+// console.log(youtubename);
+// console.log(anothername);
+
+let userone = {
+    email: "shiv123@google.com",
+    upi: "shiv@ybl"
+}
+let usertwo = userone
+console.log(userone);
+console.log(usertwo);
+usertwo.email = "shivraj@gmail.com";
+console.log(usertwo);
+
+
+
